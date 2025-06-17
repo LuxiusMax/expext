e79c44c54e6668ab3ccc5fda5f53a12c84f31592
Pavel <pavelj1983@gmail.com>
2025-06-17 21:32:16 +0300

    commit 17/6/25 - added true excel files with true data

--- expext_fun.py
+++ expext_fun.py
@@ -2,7 +2,7 @@
 
 import os
 import shutil
-import datetime
+from datetime import datetime
 import pandas as pd
 
 def frisatday():
@@ -33,24 +33,32 @@ def frisatday():
 if __name__ == "__main__":
     # Tests
 
-    # Creating paths for each step of expenses extraction
+    # Creating paths for each phase of file processing
 
     source_path = "C:\\Users\\pavel\\OneDrive\\Desktop\\My_projects\\Expenses_extraction\\expext\\SourcePath"
     process_path = "C:\\Users\\pavel\\OneDrive\\Desktop\\My_projects\\Expenses_extraction\\expext\\ProcessPath"
     done_path = "C:\\Users\\pavel\\OneDrive\\Desktop\\My_projects\\Expenses_extraction\\expext\\DonePath"
 
-    # Getting the csv files names from the "SourcePath" directory
+    # creating an empty dataframs
+
+    df = pd.DataFrame()
 
     data_source = os.listdir(source_path)
-    data_source_related = []
-    for file in data_source:
-        if ".csv" in file:
-            data_source_related.append(file)
-            print(data_source)
-            print(data_source_related)
-            shutil.move(f"{source_path}\\{data_source_related[0]}", f"{process_path}\\{data_source_related[0]}")
-            df = pd.read_csv(f"{process_path}\\{data_source_related[0]}", encoding = 'latin1')
-            print(df)
+    data_source_related = [f for f in data_source if f.endswith(".csv")]
+    for file in data_source_related:
+        shutil.move(f"{source_path}\\{file}", f"{process_path}\\{file}")         
+        df_temp = pd.read_csv(f"{process_path}\\{file}", encoding = "utf-8")
+        df_temp.
+        # df = pd.concat()
+
+        print(df_temp)
+    
+    for file in data_source_related:
+        shutil.move(f"{process_path}\\{file}", f"{source_path}\\{file}")
+    print(os.listdir(source_path))
+
+
+# %%
 
 
 
